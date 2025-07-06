fc08169b4f1b3f35790626df7ef53526d0e172f2
ArshadMQ <arshadq118@gmail.com>
2025-07-07 02:41:15 +0530

    update

--- calories-be/utils/ratelimiter.py
+++ calories-be/utils/ratelimiter.py
@@ -1,28 +1,16 @@
 from datetime import datetime, timedelta
-<<<<<<< HEAD
-from fastapi import Request, HTTPException
-=======
 from fastapi import Request
 from fastapi.responses import JSONResponse
->>>>>>> 81f30e93aa55c0c603e83f968eab34f3b8ad08e9
 from starlette.middleware.base import BaseHTTPMiddleware
 from sqlalchemy.orm import Session
 from database.connection import SessionLocal
 from database.schema import RateLimit
 
-<<<<<<< HEAD
 MAX_REQUESTS = 15
 WINDOW_MINUTES = 1
 
 class RateLimitMiddleware(BaseHTTPMiddleware):
-    def dispatch(self, request: Request, call_next):
-=======
-MAX_REQUESTS = 100
-WINDOW_MINUTES = 1
-
-class RateLimitMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
->>>>>>> 81f30e93aa55c0c603e83f968eab34f3b8ad08e9
         ip = request.client.host
         now = datetime.utcnow()
 
@@ -40,9 +28,6 @@ class RateLimitMiddleware(BaseHTTPMiddleware):
                 else:
                     if rate_limit.request_count >= MAX_REQUESTS:
                         db.close()
-<<<<<<< HEAD
-                        return {}
-=======
                         return JSONResponse(
                             status_code=429,
                             content={
@@ -51,7 +36,6 @@ class RateLimitMiddleware(BaseHTTPMiddleware):
                                 "data": {},
                             }
                         )
->>>>>>> 81f30e93aa55c0c603e83f968eab34f3b8ad08e9
                     rate_limit.request_count += 1
                     rate_limit.last_request = now
 
@@ -68,10 +52,5 @@ class RateLimitMiddleware(BaseHTTPMiddleware):
         finally:
             db.close()
 
-<<<<<<< HEAD
-        response = call_next(request)
-=======
-        # Ensure you await this!
         response = await call_next(request)
->>>>>>> 81f30e93aa55c0c603e83f968eab34f3b8ad08e9
         return response
